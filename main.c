51fed7524220baefead5e11efcb76ca65feabd9e
Killern1nd404 <andrey.vasilev05@mail.ru>
2024-02-21 21:26:59 +0300

    memory usage of vector

--- main.c
+++ main.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "libs/data_structures/bitset/bitset.h"
+#include "libs/data_structures/vector/vector.h"
 #include "OP/lab13/lab13.h"
 #include "DM/lab1_1/lab1_1.h"
 
 int main() {
-    test_bitset();
-
-    //lab13_12();
-
-    /*size_t a_size = 5;
-    size_t b_size = 6;
-    int set_a[5] = {1, 4, 3, 6, 5};
-    int set_b[6] = {1, 3, 2, 6, 8, 4};
-
-    size_t c1_size = a_size + b_size;
-    int set_c1[c1_size];
-    unordered_set_unification(set_a, a_size, set_b, b_size, set_c1, &c1_size);
-    set_print(set_c1, c1_size);*/
-
-    //calculate_task_1();
-    //check_boolean_set_operations();
+    vector v = createVector(2);
 
     return 0;
 }
