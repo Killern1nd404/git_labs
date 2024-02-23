a8a00eda2686f182d02ff80a3e2028fb8b3f36e7
Killern1nd404 <andrey.vasilev05@mail.ru>
2024-02-23 14:43:01 +0300

    init vectorVoid

--- main.c
+++ main.c
@@ -2,11 +2,17 @@
 #include <stdint.h>
 #include "libs/data_structures/bitset/bitset.h"
 #include "libs/data_structures/vector/vector.h"
+#include "libs/data_structures/vector/vectorVoid.h"
 #include "OP/lab13/lab13.h"
 #include "DM/lab1_1/lab1_1.h"
 
 int main() {
-    test();
+    vector v = createVector(2);
+    pushBack(&v, 1);
+    pushBack(&v, 2);
+    int* result = atVector(&v, 1);
+
+    vectorVoid v_void = {result, v.size, v.capacity, sizeof(int)};
 
     return 0;
 }
