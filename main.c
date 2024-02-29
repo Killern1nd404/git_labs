0830f9c30a773499d052f34e2a103a7888eae9d2
Killern1nd404 <andrey.vasilev05@mail.ru>
2024-02-29 14:29:16 +0300

    Matrix input/output

--- main.c
+++ main.c
@@ -5,10 +5,12 @@
 #include "libs/data_structures/vector/vectorVoid.h"
 #include "OP/lab13/lab13.h"
 #include "DM/lab1_1/lab1_1.h"
+#include "libs/data_structures//matrix/matrix.h"
 
 int main() {
-    //test1();
-    //test2();
+    matrix m = getMemMatrix(2, 2);
+    inputMatrix(&m);
+    outputMatrix(m);
 
     return 0;
 }
