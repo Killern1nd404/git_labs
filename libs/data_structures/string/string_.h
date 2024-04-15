b66b59bf7bb2323a37d579c3e420c36461d47dae
Killern1nd404 <andrey.vasilev05@mail.ru>
2024-04-15 21:32:43 +0300

    String_ reverse_words_order

--- libs/data_structures/string/string_.h
+++ libs/data_structures/string/string_.h
@@ -144,4 +144,8 @@ void join_strings(char *string1, char *string2, char *result);
 
 void test_join_strings();
 
+void reverse_words_order(char *string);
+
+void test_reverse_words_order();
+
 #endif //UNTITLED_STRING__H
