05c6af8dbb5cb1a172211d3047ea1f1de91df504
Killern1nd404 <andrey.vasilev05@mail.ru>
2024-05-02 00:40:39 +0300

    Files transpose_non_symmetric_matrices

--- libs/data_structures/files/files.h
+++ libs/data_structures/files/files.h
@@ -42,6 +42,10 @@ void sort_negative_after_positive(char *file_name, size_t size);
 
 void test_sort_negative_after_positive();
 
+void transpose_non_symmetric_matrices(char *file_name, size_t size);
+
+void test_transpose_non_symmetric_matrices();
+
 void test_files();
 
 #endif //UNTITLED_FILES_H
