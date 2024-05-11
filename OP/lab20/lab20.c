d877f7dae6f6345d9d982a00fc9b95b2de9ca95c
Killern1nd404 <andrey.vasilev05@mail.ru>
2024-05-11 14:28:21 +0300

    Lab_20 task_5

--- OP/lab20/lab20.c
+++ OP/lab20/lab20.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include "C:\Users\andre\CLionProjects\Project\libs\data_structures\matrix\matrix.h"
 #include <assert.h>
+#include <malloc.h>
+#include <string.h>
 
 int** task_1(int n, int query[][4], size_t size) {
     matrix result = getMemMatrix(n, n);
@@ -187,8 +189,243 @@ void test_task_3() {
     }
 }
 
+
+
+
+
+
+
+/*int search_domain(domain *results, int size, char *s) {
+    for (int i = 0; i < size; i++) {
+        if (strcmp(results[i].name, s) == 0) {
+            return i;
+        }
+    }
+
+    return size;
+}
+
+bool search_number(int *array, int size, int number) {
+    for (int i = 0; i < size; i++) {
+        if (number == array[i])
+            return true;
+    }
+
+    return false;
+}
+
+void handle_dot_pointer(domain *array, int i, char *dot_pointer, domain *results, int *results_size) {
+    strcpy(array[i].name, dot_pointer + 1);
+    int pos = search_domain(results, *results_size, array[i].name);
+    if (pos == *results_size) {
+        results[*results_size] = array[i];
+        *results_size += 1;
+    } else {
+        results[pos].visits_number += array[i].visits_number;
+    }
+}
+
+void output_domains(domain *results, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d %s\n", results[i].visits_number, results[i].name);
+    }
+}
+
+void task_4(domain *array, int size) {
+    int closes_indexes[size];
+    int closes_counter = 0;
+    domain results[100];
+    int results_size = 0;
+
+    for (int i = 0; i < size; i++)
+        results[results_size++] = array[i];
+
+    while (closes_counter != size) {
+        for (int i = 0; i < size; i++) {
+            if (!search_number(closes_indexes, closes_counter, i)) {
+                char *dot_pointer;
+                dot_pointer = strchr(array[i].name, '.');
+
+                if (dot_pointer != NULL)
+                    handle_dot_pointer(array, i, dot_pointer, results, &results_size);
+                else
+                    closes_indexes[closes_counter++] = i;
+            }
+        }
+    }
+
+    output_domains(results, results_size);
+}
+
+void test_task_4() {
+    char *cpdomains = "9001 discuss.codeforces.com";
+    domain d;
+    d.visits_number = 9001;
+    domain r[1] = {d};
+    d.name = "discuss.codeforces.com";
+    task_4(r, 1);
+}*/
+
+void fill_matrix(matrix m, matrix *new_matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (m.values[i][j] == 1) {
+                if (i != 0) {
+                    new_matrix->values[i][j] = new_matrix->values[i - 1][j] + 1;
+                } else {
+                    new_matrix->values[i][j] = 1;
+                }
+            } else {
+                new_matrix->values[i][j] = 0;
+            }
+        }
+    }
+}
+
+int task_5(matrix m, int rows, int cols) {
+    matrix new_matrix = getMemMatrix(rows, cols);
+    fill_matrix(m, &new_matrix, rows, cols);
+    int result = 0;
+
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < rows; j++) {
+            for (int k = i + 1; k < cols + 1; k++) {
+                int min = new_matrix.values[j][i];
+
+                for (int g = i; g < k; g++) {
+                    if (new_matrix.values[j][g] < min)
+                        min = new_matrix.values[j][g];
+                }
+
+                result += min;
+            }
+        }
+    }
+
+    return result;
+}
+
+void test_task_5() {
+    matrix matrix_1 = createMatrixFromArray((int[]) {
+                                              1, 0, 1,
+                                              1, 1, 0,
+                                              1, 1, 0},
+                                      3, 3);
+    int result_1 = task_5(matrix_1, 3, 3);
+    assert(result_1 == 13);
+
+    matrix matrix_2 = createMatrixFromArray((int[]) {
+                                              1, 1, 1, 0,
+                                              0, 0, 1, 1,
+                                              1, 0, 0, 0},
+                                      3, 4);
+    int result_2 = task_5(matrix_2, 3, 4);
+    assert(result_2 == 11);
+}
+
+
+
+/*int searchMaxIdx(const int array[], int start, int end) {
+    if (start > end) {
+        return end + 1;
+    } else {
+        int max_num_idx = start;
+        for (int ind = start + 1; ind <= end; ind++) {
+            if (array[ind] > array[max_num_idx])
+                max_num_idx = ind;
+        }
+
+        return max_num_idx;
+    }
+}
+
+root_node *create_root_node(int number) {
+    root_node *result = (root_node *) malloc(sizeof(root_node));
+    result->max_value = number;
+    result->left_prefix = NULL;
+    result->right_prefix = NULL;
+
+    return result;
+}
+
+root_node *insert(root_node *node, int number, bool is_left) {
+    root_node *result = create_root_node(number);
+    if (is_left) {
+        node->left_prefix = result;
+    } else {
+        node->right_prefix = result;
+    }
+
+    return result;
+}
+
+
+void create_root_nodes(root_node *node, int *nums, int start, int end, bool is_left) {
+    /*int max_number = nums[0];
+    int index = 0;
+
+    for (int i = 1; i <= end; i++) {
+        if (nums[i] > max_number) {
+            max_number = nums[i];
+            index = i;
+        }
+    }*/
+
+    /*int max_number_index = searchMaxIdx(nums, start, end);
+
+    if (max_number_index == end + 1) {
+        if (is_left) {
+            node->left_prefix = NULL;
+        } else {
+            node->right_prefix = NULL;
+        }
+
+        printf("null ");
+        return;
+    }
+
+    printf("%d ", nums[max_number_index]);
+    root_node *new_node = insert(node, nums[max_number_index], is_left);
+    create_root_nodes(new_node, nums, start, max_number_index - 1, true);
+    create_root_nodes(new_node, nums, max_number_index + 1, end, false);
+}
+
+void task_7(int *nums, int size) {
+    if (size == 0) {
+        return;
+    }
+
+    /*int max_number = nums[0];
+    int index = 0;
+
+    for (int i = 1; i < size; i++) {
+        if (nums[i] > max_number) {
+            max_number = nums[i];
+            index = i;
+        }
+    }*/
+
+    /*int max_number_index = searchMaxIdx(nums, 0, size - 1);
+
+    root_node *new_node = create_root_node(nums[max_number_index]);
+    printf("%d ", nums[max_number_index]);
+
+    create_root_nodes(new_node, nums, 0, max_number_index - 1, true);
+    create_root_nodes(new_node, nums, max_number_index + 1, size - 1, false);
+
+    printf("\n");
+}
+
+void test_task_7() {
+    int nums[6] = {3, 2, 1, 6, 0, 5};
+    task_7(nums, 6);
+}*/
+
 void test_lab20() {
     //test_task_1();
     //test_task_2();
-    test_task_3();
+    //test_task_3();
+    //test_task_4();
+    test_task_5();
+    //test_task_7();
 }
--- OP/lab20/lab20.h
+++ OP/lab20/lab20.h
@@ -4,6 +4,17 @@
 #include <stdio.h>
 #include "C:\Users\andre\CLionProjects\Project\libs\data_structures\matrix\matrix.h"
 
+/*typedef struct domain {
+    int visits_number;
+    char *name;
+} domain;*/
+
+/*typedef struct root_node {
+    int max_value;
+    struct root_node *left_prefix;
+    struct root_node *right_prefix;
+} root_node;*/
+
 int** task_1(int n, int query[][4], size_t size);
 
 void test_task_1();
@@ -19,6 +30,24 @@ void get_median(int filter, int matrix[filter][filter], int i, int j);
 void task_3(int filter, int matrix[filter][filter]);
 
 void test_task_3();
+
+//void test_task_4();
+
+void fill_matrix(matrix m, matrix *new_matrix, int rows, int cols);
+
+int task_5(matrix m, int rows, int cols);
+
+void test_task_5();
+
+/*root_node *insert(root_node *node, int number, bool is_left);
+
+root_node *create_root_node(int number);
+
+void create_root_nodes(root_node *node, int *nums, int start, int end, bool is_left);
+
+void task_7(int *nums, int size);
+
+void test_task_7();*/
 
 void test_lab20();
 
